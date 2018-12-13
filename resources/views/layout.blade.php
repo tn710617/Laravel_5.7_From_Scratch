afb4b3d4725e3cac7feeb72d9ac192533301f41f
Ray <tn710617@gmail.com>
2018-12-13 15:52:35 +0800

    Create a layout and extends it, also complete yield and section function

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Us</title>
+</head>
+<body>
+<ul>
+    <li><a href="/">Welcome</a></li>
+    <li><a href="/about">About</a></li>
+    <li><a href="/contact">contact</a></li>
+</ul>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('content')
+   <h1>Welcome here</h1>
+    @endsection
