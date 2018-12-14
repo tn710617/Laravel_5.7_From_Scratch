a2bb36ccf6e4aef99a6757182d7bd9e6c382a8b8
Ray <tn710617@gmail.com>
2018-12-14 15:40:31 +0800

    Episode 8, Use MVC concept, passing data from controller to view

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::all();
+        return view('projects', compact('projects'));
+    }
+}
